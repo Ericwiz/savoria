7c92ab3b5a664bae5e6572c6bd705916251ce33d
Ericwiz <wisdomagu44@gmail.com>
2023-09-07 11:09:43 +0100

    fix: fixed header image swiper display on small screen

--- components/landingPage.vue
+++ components/landingPage.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="h-screen">
+    <header class="sm:h-screen">
         <div class="flex flex-col justify-between items-center sm:flex-row md:flex-row-reverse w-full h-full">
-            <div class="h-full bg-black text-white py-20 px-7 w-full sm:w-[50%] flex flex-col">
+            <div class="bg-black text-white py-20 px-7 h-full w-full sm:w-[50%] flex flex-col">
                 <h1 class="text-lg xs:text-2xl font-bold text-center font-mono">Delicious Delights Delivered to Your
                     Doorstep</h1>
                 <p class=" text-sm xs:text-lg text-center font-mono py-10">Welcome to Savoria, your one-stop destination for
@@ -13,11 +13,11 @@
                     View our Menu </button>
             </div>
             <!-- <div > -->
-            <Swiper :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true" :effect="'creative'"
-                :autoplay="{
-                    delay: 4000,
-                    disableOnInteraction: true,
-                }" :creative-effect="{
+                <Swiper :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true"
+                    :effect="'creative'" :autoplay="{
+                        delay: 4000,
+                        disableOnInteraction: true,
+                    }" :creative-effect="{
     prev: {
         shadow: false,
         translate: ['-20%', 0, -1],
@@ -25,20 +25,22 @@
     next: {
         translate: ['100%', 0, 0],
     },
-}" class="hidden sm:block h-full sm:w-[50%]">
-                <SwiperSlide>
-                    <img src="/headerImg03.jpg" alt="Header Image" class="h-full w-full">
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/headerImg01.jpg" alt="Header Image" class="h-full w-full">
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/headerImg02.jpg" alt="Header Image" class="h-full w-full">
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src="/headerImg04.jpg" alt="Header Image" class="h-full w-full">
-                </SwiperSlide>
-            </Swiper>
+}" class="w-full h-0 hidden sm:h-full sm:w-[50%]">
+                    <SwiperSlide>
+                        <img src="/headerImg03.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg01.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg02.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg04.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                </Swiper>
+            <!-- </div> -->
+
         </div>
     </header>
     <main>
@@ -64,7 +66,7 @@
         </div>
         <div v-else>
             <Menus>
-                <h2 class="text-center text-2xl">Our Menus</h2>
+                <h2 class="text-center text-2xl pt-3 pb-8">Our Menus</h2>
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                     <Card v-for="menu in menus" :key="menu.id" :image="menu.menu_image" :title="menu.menu_name"
                         :content="menu.menu_description" />
---
+++ components/swipe.vue
@@ -0,0 +1,36 @@
+<template>
+    <Swiper :modules="[SwiperAutoplay, SwiperEffectCreative]" :slides-per-view="1" :loop="true"
+                    :effect="'creative'" :autoplay="{
+                        delay: 4000,
+                        disableOnInteraction: true,
+                    }" :creative-effect="{
+    prev: {
+        shadow: false,
+        translate: ['-20%', 0, -1],
+    },
+    next: {
+        translate: ['100%', 0, 0],
+    },
+}">
+                    <SwiperSlide>
+                        <img src="/headerImg03.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg01.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg02.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src="/headerImg04.jpg" alt="Header Image" class="h-full w-full">
+                    </SwiperSlide>
+                </Swiper>
+  </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
